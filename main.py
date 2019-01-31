2821777b3ec96a67cac8f068e7a2e3ce8c06e223
Akashdeep Deb <deb2@illinois.edu>
2019-01-31 09:09:09 -0600

    basic

---
+++ main.py
@@ -0,0 +1,13 @@
+#basic impl (move to node)
+
+def main():
+    query = input('What do you want to learn about? ')
+    N = input('How many related articles do you want to read? ')
+
+    # find wikipedia root for the query
+
+
+    # and then find top N similar articles
+    return query, N
+
+main()
